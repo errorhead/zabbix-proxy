b8e525e8fb94caf6de0ecfe4a1333309b2a01297
root <root@proxy.dsg.local>
2020-03-05 00:08:59 +0100

    changes...

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ################################################################################################
-# Deploy zabbix proxy with psql db (Postgres)						       #
-# developed by tmiklu v1.0								       #
-# created for DSG 				                                               #
+# Deploy zabbix proxy with psql db (Postgres)                                                  #
+# developed by tmiklu v1.0					                               #
+# created for Descartes systems  				                               #
 ################################################################################################
 
 clear
@@ -91,16 +91,45 @@ case $USERINPUT in
 	# backup config
     echo "backup config to /var/lib/pgsql/data/postgresql.conf.bak"
     cp -pa /var/lib/pgsql/data/postgresql.conf /var/lib/pgsql/data/postgresql.conf.bak
+    cp -pa /var/lib/pgsql/data/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf.bak
 
 	echo "changing config with sed."
-    sed -ie "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /var/lib/pgsql/data/postgresql.conf
-    sed -ie 's/#port = 5432/port = 5432/g' /var/lib/pgsql/data/postgresql.conf
+    sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /var/lib/pgsql/data/postgresql.conf
+    sed -i 's/#port = 5432/port = 5432/g' /var/lib/pgsql/data/postgresql.conf
+
+    sed -i 's/local/#local/g' /var/lib/pgsql/data/pg_hba.conf
+    sed -i 's/host/#host/g' /var/lib/pgsql/data/pg_hba.conf
+    echo "local all all trust" >> /var/lib/pgsql/data/pg_hba.conf
+    echo "host all all 127.0.0.1/32 trust" >> /var/lib/pgsql/data/pg_hba.conf
+    echo "host all all ::1/128 trust" >> /var/lib/pgsql/data/pg_hba.conf
+
+    sed -i 's/Hostname=Zabbix proxy/Hostname=proxy/g' /etc/zabbix/zabbix_proxy.conf
+    sed -i 's/Server=127.0.0.1/Server=192.168.56.130/g' /etc/zabbix/zabbix_proxy.conf
+    sed -i 's/DBName=zabbix_proxy/DBName=zabbix/g' /etc/zabbix/zabbix_proxy.conf
+    sed -i 's/DBUser=zabbix/DBUser=zabbix/g' /etc/zabbix/zabbix_proxy.conf
+    sed -i 's/# DBPassword=/DBPassword=zabbix/g' /etc/zabbix/zabbix_proxy.conf
+    sed -i 's/# DBPort=/DBPort=5432/g' /etc/zabbix/zabbix_proxy.conf
+
 
     # enable and start zabbix proxy
     systemctl enable zabbix-proxy && systemctl start zabbix-proxy 
     
     # enable and start pgsql
     systemctl enable postgresql.service && systemctl start postgresql.service
+
+    # create database, user zabbix and grant privileges
+    sudo su postgres <<EOF
+    psql -c "alter user postgres with password 'zabbix';"
+    psql -c "create user zabbix;"
+    psql -c "create database zabbix owner zabbix;"
+    psql -c "alter user zabbix with password 'zabbix';"
+    psql -c "grant all privileges on database zabbix to zabbix;"
+EOF
+
+    systemctl restart postgresql.service
+
+    # unzip schema content to postgres
+    zcat /usr/share/doc/zabbix-proxy-pgsql-4.0.18/schema.sql.gz | psql -U zabbix -d zabbix
 
     ;;
   
